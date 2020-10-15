1b40f0741eb76ee6133a8bef31b42e8b61e2b1c2
Rotem <rotemjordan@gmail.com>
2020-10-15 17:10:43 +0200

    my first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+const person = {
+  name: "Anna",
+  yearBorn: 1992,
+  likeSports: false,
+  pets:['dog', 'cat', 'elephant'] 
+}
+
+function readPersonMind(person){
+  const date = new Date();
+  const msg = `Welcome ${person.name}!
+          I can tell by my mysterious powers that your name is spelt with ${person.name.length} letters,
+          you are ${date.getFullYear() - person.yearBorn} years old,
+          you ${person.likeSports ? " ": "don't "}like sports, and have ${person.pets.length} pets.`
+  return msg;
+}
+
+function App() {
+  return (
+  <p className="multiline">{readPersonMind(person)}</p>
+  );
+}
+
+export default App;
